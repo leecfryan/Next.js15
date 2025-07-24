9480bfe549923fcde8e978cb980f8b29e5631187
PewPew <61502605+leecfryan@users.noreply.github.com>
2025-07-24 20:40:01 +0800

    working on the child prop

--- React Refresher/react-crash-course/src/components/PostsList.jsx
+++ React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -1,11 +1,34 @@
-import Post from './Post'
-import classes from './PostsList.module.css';
+import NewPost from "./NewPost";
+import Post from "./Post";
+import classes from "./PostsList.module.css";
+import { useState } from "react";
 
-function PostsList(){
-return <ul className={classes.posts}>
-      <Post author="Maximillian" body="React.js is awesome!"/>
-      <Post author="Manuel" body="Check out the full course!"/>
-</ul>
+function PostsList() {
+  const [enteredBody, setEnteredBody] = useState(""); // state for the body text
+
+  const [enteredAuthor, setEnteredAuthor] = useState(""); // state for the author name
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;
