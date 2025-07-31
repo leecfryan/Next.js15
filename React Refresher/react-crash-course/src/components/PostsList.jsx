f64fd9ae11142e71626a6f143ed481036e5d110e
PewPew <61502605+leecfryan@users.noreply.github.com>
2025-07-31 18:22:31 +0800

    Moving onto routing

--- React Refresher/react-crash-course/src/components/PostsList.jsx
+++ React Refresher/react-crash-course/src/components/PostsList.jsx
@@ -2,12 +2,38 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
+  // fetch("http://localhost:8080/posts")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     setPosts(data.posts);
+  //   });
+
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
@@ -19,7 +45,7 @@ function PostsList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
@@ -27,10 +53,16 @@ function PostsList({ isPosting, onStopPosting }) {
         </ul>
       )}
 
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>No posts yet</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
